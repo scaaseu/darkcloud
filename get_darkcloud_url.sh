78c8f7a8734ded8e5a65dad9025ba749d8dd7719
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-16 09:19:11 +0000

    work in progress

--- get_darkcloud_url.sh
+++ get_darkcloud_url.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-docker exec darkcloud-onionize cat /var/lib/tor/onion_services/winter_is_coming/hostname
+echo hidden address of IDS
+docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-ids/hostname
+
+echo hidden address of SOC
+docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-soc/hostname
+
+echo hidden address of relational database
+docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-db/hostname
+
+echo hidden address of darkcloud service
+docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud/hostname
+
 
