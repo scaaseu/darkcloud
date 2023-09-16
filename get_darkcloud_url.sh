30a5847d402439c998513d0414d3ea64ba3e7aec
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-16 11:41:38 +0000

    volume handling

--- get_darkcloud_url.sh
+++ get_darkcloud_url.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo hidden address of darkcloud service
-docker exec darkcloud-onionize cat /var/lib/tor/onion_services/nextcloud/hostname
+docker exec darkcloud-onionize cat /var/lib/tor/onion_services/winter_is_coming/hostname
