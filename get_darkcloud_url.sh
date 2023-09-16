c52494359cd0b1728a03ad525eba27a83fb58f24
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-16 09:49:15 +0000

    testing

--- get_darkcloud_url.sh
+++ get_darkcloud_url.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-echo hidden address of IDS
-docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-ids/hostname
-
-echo hidden address of SOC
-docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-soc/hostname
-
-echo hidden address of relational database
-docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud-db/hostname
-
 echo hidden address of darkcloud service
 docker exec darkcloud-onionize cat /var/lib/tor/onion_services/darkcloud/hostname
-
-
